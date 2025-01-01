4490a00800f98e37cf8a341e2b9412256fb63cfe
alfianekrhmn <alfian.eka481@gmail.com>
2025-01-01 21:13:23 +0700

    Memahami Logika Dasar dengan JavaScript

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,9 +6,34 @@ function btn() {
     let fullName = document.getElementById("fullName").value
     let email = document.getElementById("email").value
     let massage = document.getElementById("massage").value
-    console.log(`fullname: ${fullName}`)
-    console.log(`Email: ${email}`)
-    console.log(`Massage: ${massage}`)
+    if(fullName == '' || email == '' || massage == ''){
+        document.querySelector("#messeges").style.display = "block"
+        document.querySelector("#messeges").style.color = "red"
+        document.querySelector("#messeges").style.padding = "20px"
+        document.querySelector("#messeges").style.backgroundColor = "white"
+        document.querySelector("#messeges").innerHTML = "Silakan lengkapi formulirnya"
+
+    }else{
+        function validE(email) {
+            const patt = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            return patt.test(email)
+        }
+        
+        if(validE(email)){
+            document.querySelector("#messeges").style.display = "block"
+            document.querySelector("#messeges").style.color = "green"
+            document.querySelector("#messeges").style.padding = "20px"
+            document.querySelector("#messeges").style.backgroundColor = "white"
+            document.querySelector("#messeges").innerHTML = "Terima kasih sudah mendaftar"
+            alert("Terima kasih sudah mendaftar")
+        }else{
+            document.querySelector("#messeges").style.display = "block"
+            document.querySelector("#messeges").style.color = "red"
+            document.querySelector("#messeges").style.padding = "20px"
+            document.querySelector("#messeges").style.backgroundColor = "white"
+            document.querySelector("#messeges").innerHTML = "Silakan perbaiki emailnya"
+        }
+    }
 }
 
 
